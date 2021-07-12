317d5ead5d2f5e25d9dde8c4fe496e4b35551532
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-07-12 11:13:39 +0700

    add config heroku

--- setup.js
+++ setup.js
@@ -4,11 +4,13 @@ const { exec } = require("child_process");
 const path = process.argv[1];
 const type = process.argv[2];
 
-const setupAndroid = () => {
-	const androidConfigPath = path.slice(0, path.lastIndexOf("/") + 1) + "android/app/src/main/res/raw/config.properties";
-
-	console.log('androidConfigPath', androidConfigPath)
-
+const getURL = (callback) => {
+	const { url } = require('./backend/config.json');
+	if (url) {
+		callback?.(url);
+		return
+	}
+	//get local url
 	exec("ipconfig getifaddr en0", (error, ip, stderr) => {
 		if (error) {
 			console.log(`error: ${error.message}`);
@@ -18,35 +20,29 @@ const setupAndroid = () => {
 			console.log(`stderr: ${stderr}`);
 			return;
 		}
+		callback?.(`http://${ip.slice(0, -1)}:3000`) 		//erase lineFeed
+	});
+}
 
-		const conetent = `socket_url=http://${ip.slice(0, -1)}:3000`		//erase lineFeed
-
+const setupAndroid = () => {
+	const androidConfigPath = path.slice(0, path.lastIndexOf("/") + 1) + "android/app/src/main/res/raw/config.properties";
+	console.log('androidConfigPath', androidConfigPath)
+	getURL((url) => {
+		const conetent = `socket_url=${url}`
 		fs.writeFile(androidConfigPath, conetent, "utf8", (err) => {
 			if (err) {
 				console.error(err)
 			}
 			console.log('config url for android sucessfully')
 		})
-	});
+	})
 }
 
 const setupIOS = () => {
 	const iosConfigPath = path.slice(0, path.lastIndexOf("/") + 1) + "ios/Canvas-iOS/Utilities/Configs.swift";
-
 	console.log('iosConfigPath', iosConfigPath)
-
-	exec("ipconfig getifaddr en0", (error, ip, stderr) => {
-		if (error) {
-			console.log(`error: ${error.message}`);
-			return;
-		}
-		if (stderr) {
-			console.log(`stderr: ${stderr}`);
-			return;
-		}
-
-		const conetent = `public let SOCKET_URI = "http://${ip.slice(0, -1)}:3000";`		//erase lineFeed
-
+	getURL((url) => {
+		const conetent = `public let SOCKET_URI = "${url}";`
 		fs.writeFile(iosConfigPath, conetent, "utf8", (err) => {
 			if (err) {
 				console.error(err)
